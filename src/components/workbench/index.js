d3770943033b61bef12ca1e15ef9840b615f3362
JIWAN KIM <jiwan0123@naver.com>
2022-10-08 01:41:46 +0900

    커밋 테스트 2

--- src/components/workbench/index.js
+++ src/components/workbench/index.js
@@ -1,26 +1,23 @@
-import { useState } from "react";
-import useWorkbench from "../../hook/useWorkbench";
+import { useState } from 'react'
+import useWorkbench from '../../hook/useWorkbench'
 
 function Workbench(props) {
-    
-    const [workbench, setWorkbench] = useWorkbench();        
-    //{ workbench.tabs[0].blocks[0] }
+  const [workbench, setWorkbench] = useWorkbench()
+  //{ workbench.tabs[0].blocks[0] }
 
-    
+  //커밋 위해 테스트
 
-    return (
-        <div className='workbench'>
-          <div className='tabs'>
-            <span className='tab-item'>TAB 1</span>
-            <span className='tab-item'>TAB 2</span>
-            <span className='tab-item'>TAB 3</span>
-            <span className='tab-item'>TAB 4</span>
-          </div>
-          <div>
-            { workbench.tabs[0].blocks[0] }
-          </div>
-        </div>
-    );
+  return (
+    <div className="workbench">
+      <div className="tabs">
+        <span className="tab-item">TAB 1</span>
+        <span className="tab-item">TAB 2</span>
+        <span className="tab-item">TAB 3</span>
+        <span className="tab-item">TAB 4</span>
+      </div>
+      <div>{workbench.tabs[0].blocks[0]}</div>
+    </div>
+  )
 }
 
-export default Workbench;
+export default Workbench
