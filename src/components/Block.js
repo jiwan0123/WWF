634300e6a2c254d85fb40cafb193548f81ecdcb4
JIWAN KIM <jiwan0123@naver.com>
2022-10-08 01:30:11 +0900

    kjw branch first commit

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,17 +1,11 @@
 export default {
-    Assignment : function() {
-        return(
-            <div>네모이다</div>
-        );
-    },
-    Start : function() {
-        return(
-            <div>시작이다</div>
-        );
-    },
-    End : function() {
-        return(
-            <div>끝이다</div>
-        );
-    }
+  Assignment: function () {
+    return <div>네모이다</div>
+  },
+  Start: function () {
+    return <div>시작이다(원임)</div>
+  },
+  End: function () {
+    return <div>끝이다</div>
+  },
 }
